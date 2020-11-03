09f6b5cfa4ea2809d155c55a402b5f5008d1ac64
pikktorr <viktor.rozgonyi@gmail.com>
2020-11-03 21:04:18 +0100

    minor styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,12 +3,10 @@ import React from "react";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, subtitle, imageUrl, size }) => (
-  <div
-    className={size ? `${size} menu-item` : "menu-item"}
-    style={{
+  <div className={size ? `${size} menu-item` : "menu-item"}>
+    <div className="background-image" style={{
       backgroundImage: `url(${imageUrl})`,
-    }}
-  >
+    }}></div>
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">{subtitle}</span>
