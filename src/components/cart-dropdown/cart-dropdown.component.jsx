78748e63177cf2464fbdaa3341d2f4024c944e55
pikktorr <viktor.rozgonyi@gmail.com>
2021-01-18 20:54:31 +0100

    increase and decrease the quantities of items on checkout page and on dropdown menu

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,23 +4,30 @@ import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router-dom";
 
 import CustomButton from "../custom-button/custom-button.component";
-import CartItem from "../cart-item/cart-item.component";
+// import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { toggleCartHidden } from "../../redux/cart/cart.action";
 
 import "./cart-dropdown.styles.scss";
+import CheckoutItemComponent from "../checkout-item/checkout-item.component";
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
         cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
+          <CheckoutItemComponent key={cartItem.id} cartItem={cartItem} />
         ))
       ) : (
         <p className="empty-message">Your cart is empty</p>
       )}
     </div>
-    <CustomButton onClick={() => history.push("/checkout")}>
+    <CustomButton
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toggleCartHidden());
+      }}
+    >
       Go to Checkout
     </CustomButton>
   </div>
@@ -29,5 +36,7 @@ const CartDropdown = ({ cartItems, history }) => (
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
 });
+
+// mapDispatchToProps nélkül is megy, ha használjuk a dispatch-et fent
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,25 +1,40 @@
 import { CartActionTypes } from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
   cartItems: [],
 };
 
-const cartReducer = (currentState = INITIAL_STATE, action) => {
+const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
-        ...currentState,
-        hidden: !currentState.hidden,
+        ...state,
+        hidden: !state.hidden,
       };
+
     case CartActionTypes.ADD_ITEM:
       return {
-        ...currentState,
-        cartItems: addItemToCart(currentState.cartItems, action.payload),
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
+      };
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
       };
     default:
-      return currentState;
+      return state;
   }
 };
 
